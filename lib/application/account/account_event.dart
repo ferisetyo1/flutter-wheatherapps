e4aaf791785317dd58478f1414ee9d24120798ce
feri-setyo <ferisetyo1@gmail.com>
2022-04-28 11:15:05 +0700

    remove bloc account

--- lib/application/account/account_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'account_bloc.dart';
-
-@freezed
-class AccountEvent with _$AccountEvent {
-  const factory AccountEvent.started() = _Started;
-  const factory AccountEvent.showpass() = _Showpass;
-}
--- lib/application/account/account_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'account_bloc.dart';
-
-@freezed
-class AccountState with _$AccountState {
-  const factory AccountState({ required bool showpass}) = _AccountState;
-  factory AccountState.initial()=>AccountState(showpass: false);
-}
